6c06fb2087cd042fe9f59b434b68469565250794
hanzala-sohrab <sorb.hanzala@gmail.com>
2021-03-02 21:02:36 +0530

    Updated urlpatterns

--- api/urls.py
+++ api/urls.py
@@ -4,8 +4,8 @@ from api import views
 urlpatterns = [
     path('list/<int:id>/', views.ListByID.as_view(), name='list_id'),
     # path('list/<str:name>/', views.ListByName.as_view(), name='list_name'),
-    path('list/<str:type>/', views.ListByType.as_view(), name='list_type'),
-    path('list/<str:size>/', views.ListBySize.as_view(), name='list_size'),
+    path('list/type/<str:type>/', views.ListByType.as_view(), name='list_type'),
+    path('list/size/<str:size>/', views.ListBySize.as_view(), name='list_size'),
     path('create/pizza/', views.CreatePizza.as_view(), name='create_pizza'),
     path('create/topping/', views.CreateTopping.as_view(), name='create_topping'),
     path('modify/<int:pk>/', views.Modify.as_view(), name='modify'),
